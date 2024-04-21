4527667defc48d999ec3061cc8bd1fac37f8f5d8
정회민 <wony1102@naver.com>
2024-04-21 19:40:34 +0900

    feat(electron-app): 트레이 아이콘 추가

--- pass-admin-app/src/background.js
+++ pass-admin-app/src/background.js
@@ -1,8 +1,14 @@
 "use strict";
 
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Tray, Menu } from "electron";
 
 let win = null;
+let tray = null;
+
+const destroyApp = () => {
+  if (tray && !tray.isDestroyed()) tray.destroy();
+  app.quit();
+};
 
 const createWindow = async () => {
   win = new BrowserWindow({
@@ -17,6 +23,29 @@ const createWindow = async () => {
   });
   await win.loadURL("http://localhost:8080");
 };
+
+const createTray = () => {
+  if (tray === null) {
+    tray = new Tray("src/assets/logo.png");
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: "메인 열기",
+        type: "normal",
+        click: () => {
+          win.show();
+        },
+      },
+      { label: "종료", type: "normal", click: destroyApp },
+    ]);
+
+    tray.setToolTip("패스어드민");
+    tray.setContextMenu(contextMenu);
+  }
+};
+
+app.whenReady().then(() => {
+  createTray();
+});
 
 // app.on("window-all-closed", () => {
 //   if (process.platform !== "darwin") {
